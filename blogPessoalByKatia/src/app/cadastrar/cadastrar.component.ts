bbfe1eb155e7bea6a3029cdb4d0a4b2b80283eab
katiabianconi1 <katiabianconi@hotmail.com>
2021-06-01 20:23:11 -0300

    Consumo de API - Login e Cadastro

--- blogPessoalByKatia/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoalByKatia/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User // as variaves sempre são declaradas em cima dos construtores
+  confirmarSenha: string
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  constructor( //tudo que colocamos dentro do contrutor é injeção de dependencia
+    private authService: AuthService, //metodo de cadastrar depende do meu AuthService
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0) //colocamos isso nas paginas por padrão
   }
 
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar(){
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmarSenha){
+      alert('As senhas estão incorretas.')
+    } else { //subscribe = assim ele vai subescrever o meu objeto ts e transforma-lo em objeto json // vai enviar para rota entrar
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp 
+        this.router.navigate(['/entrar']) 
+        alert('Usuário cadastrado com sucesso!')
+      }) 
+    }
+  }
 }
