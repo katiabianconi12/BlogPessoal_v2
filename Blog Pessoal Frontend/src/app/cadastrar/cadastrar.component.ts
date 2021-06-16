daa644db54fb0ad954afd5f0229b942b7de491d4
katiabianconi1 <katiabianconi@hotmail.com>
2021-06-15 22:58:10 -0300

    Alerttas reutilizaveis implementados

--- Blog Pessoal Frontend/src/app/cadastrar/cadastrar.component.ts
+++ Blog Pessoal Frontend/src/app/cadastrar/cadastrar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../model/User';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -16,7 +17,9 @@ export class CadastrarComponent implements OnInit {
 
   constructor( //tudo que colocamos dentro do contrutor é injeção de dependencia
     private authService: AuthService, //metodo de cadastrar depende do meu AuthService
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
+
   ) { }
 
   ngOnInit() {
@@ -35,12 +38,12 @@ export class CadastrarComponent implements OnInit {
     this.user.tipo = this.tipoUsuario
 
     if(this.user.senha != this.confirmarSenha){
-      alert('As senhas estão incorretas.')
+      this.alertas.showAlertDanger('As senhas estão incorretas.')
     } else { //subscribe = assim ele vai subescrever o meu objeto ts e transforma-lo em objeto json // vai enviar para rota entrar
       this.authService.cadastrar(this.user).subscribe((resp: User) => {
         this.user = resp 
         this.router.navigate(['/entrar']) 
-        alert('Usuário cadastrado com sucesso!')
+        this.alertas.showAlertSuccess('Usuário cadastrado com sucesso!')
       }) 
     }
   }
